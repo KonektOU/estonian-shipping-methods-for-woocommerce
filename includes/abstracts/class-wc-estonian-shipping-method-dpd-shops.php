86d3a5df3f52b9b4befd173c38a01e07248b3c1d
Risto Niinemets <risto.niinemets@gmail.com>
2022-11-29 08:27:21 +0200

    Replace DPD terminals source URL

--- includes/abstracts/class-wc-estonian-shipping-method-dpd-shops.php
+++ includes/abstracts/class-wc-estonian-shipping-method-dpd-shops.php
@@ -16,7 +16,7 @@ abstract class WC_Estonian_Shipping_Method_DPD_Shops extends WC_Estonian_Shippin
 	 *
 	 * @var string
 	 */
-	public $terminals_url = 'ftp://ftp.dpd.ee/parcelshop/psexport_latest.csv';
+	public $terminals_url = 'ftp://ftp:@ftp.dpdbaltics.com/PickupParcelShopData.json';
 
 	/**
 	 * Class constructor
@@ -24,52 +24,54 @@ abstract class WC_Estonian_Shipping_Method_DPD_Shops extends WC_Estonian_Shippin
 	public function __construct() {
 		$this->terminals_template = 'dpd';
 
-		// Checkout phone numbe validation
+		// Checkout phone numbe validation.
 		add_action( 'woocommerce_after_checkout_validation', array( $this, 'validate_customer_phone_number' ), 10, 1 );
 
-		// Construct parent
+		// Construct parent.
 		parent::__construct();
 	}
 
 	/**
-	 * Fetch the terminals from remote URL
+	 * Fetch the terminals from remote URL.
+	 *
+	 * @param false|string $filter_country Country to be filtered.
+	 * @param integer      $filter_type    Additional filter. Not used for this method.
+	 *
+	 * @return array Terminals.
 	 */
 	public function get_terminals( $filter_country = false, $filter_type = 0 ) {
-		// Fetch terminals from cache
+		// Fetch terminals from cache.
 		$cached_terminals = $this->get_terminals_cache();
 
-		if( $cached_terminals !== null ) {
+		if ( null !== $cached_terminals ) {
 			return $cached_terminals;
 		}
 
-		$filter_country    = $filter_country ? $filter_country : $this->get_shipping_country();
-		$locations         = array();
+		$filter_country = $filter_country ? $filter_country : $this->get_shipping_country();
+		$locations      = array();
 
-		// Fetch
+		// Fetch terminals.
 		$terminals_request = $this->request_remote_url( $this->terminals_url );
 
-		if( $terminals_request['success'] === true ) {
-			foreach( str_getcsv( $terminals_request['data'], "\n" ) as $row_data ) {
-				$data = str_getcsv( $row_data, '|' );
+		if ( true === $terminals_request['success'] ) {
+			$terminals = json_decode( $terminals_request['data'] );
 
-				$shop_location_id = $data[22];
-				$shop_country     = substr( $shop_location_id, 0, 2 );
-
-				if( $filter_country != $shop_country ) {
+			foreach ( $terminals as $data ) {
+				if ( $filter_country !== $data->countryCode ) {
 					continue;
 				}
 
-				$locations[]      = (object) array(
-					'place_id'   => $shop_location_id,
-					'zipcode'    => $data[4],
-					'name'       => utf8_encode( $data[2] ),
-					'address'    => utf8_encode( $data[3] ),
-					'city'       => utf8_encode( $data[5] )
+				$locations[] = (object) array(
+					'place_id' => $data->parcelShopId,
+					'zipcode'  => $data->zipCode,
+					'name'     => $data->companyName,
+					'address'  => sprintf( '%s, %s', $data->street, $data->city ),
+					'city'     => $data->city,
 				);
 			}
 		}
 
-		// Save cache
+		// Save terminals to cache.
 		$this->save_terminals_cache( $locations );
 
 		return $locations;
